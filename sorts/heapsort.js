717e831ad37e032d0099bde1223bd8684a2af771
christoffer <christoffer.luccas@gmail.com>
2015-12-11 11:36:48 -0200

    primeiro commit

---
+++ sorts/heapsort.js
@@ -0,0 +1,5 @@
+'use strict';
+var Heapsort = {
+	heapsport : function() {
+	}
+};
---
+++ trees/BST.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var node = {
+	value: 0
+	,left: null
+	,right: null
+	,node: function(value, nodeLeft, nodeRight) {
+		this.value 	= value;
+		this.left 	= nodeLeft;
+		this.right 	= nodeRight;
+	}
+};
+
+var root;
+
+var BST = function(nodeRoot) {
+
+};
